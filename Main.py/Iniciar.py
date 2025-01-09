949471522451d375dad678bd712ba0d80d220086
danilo <danilosoaressantos4@gmail.com>
2025-01-09 11:00:35 -0300

    Tela inicial Melhorias na tela inicial

--- Main.py/Iniciar.py
+++ Main.py/Iniciar.py
@@ -1,10 +1,10 @@
 import tkinter as tk
+import Cadastro
 
 class MenuPrincipal:
-    def __init__(self, master, app):
+    def __init__(self, master):
         """Tela de Menu Principal"""
         self.master = master
-        self.app = app
         self.frame = tk.Frame(master)
         self.frame.pack()
 
@@ -12,46 +12,36 @@ class MenuPrincipal:
         tk.Label(self.frame, text="Menu Principal", font=("Arial", 20)).pack(pady=20)
 
         # Botões
-        tk.Button(self.frame, text="Cadastrar Pessoa", font=("Arial", 14), width=20, 
-                  command=self.app.abrir_tela_cadastro).pack(pady=10)
-        tk.Button(self.frame, text="Visualizar Registros", font=("Arial", 14), width=20, 
-                  command=self.app.abrir_tela_Registados).pack(pady=10)
+        tk.Button(self.frame, text="Cadastro de Dizimista", font=("Arial", 14), width=20, 
+                  command=self.cadastrar_dizimista).pack(pady=10)
+        tk.Button(self.frame, text="Visualizar Pagamento", font=("Arial", 14), width=20, 
+                  command=self.visualizar_pagamento).pack(pady=10)
+        tk.Button(self.frame, text="Efetuar Pagamento", font=("Arial", 14), width=20, 
+                  command=self.efetuar_pagamento).pack(pady=10)
         tk.Button(self.frame, text="Sair", font=("Arial", 14), width=20, 
                   command=self.master.quit).pack(pady=10)
+
+    def cadastrar_dizimista(self):        
+        cadastro = Cadastro.Cadastro()
+        tela_cadastro = Cadastro.TelaCadastro(self.master, cadastro)
+        self.destruir()
+
+    def visualizar_pagamento(self):
+        print("Visualizar Pagamento clicado!")
+
+    def efetuar_pagamento(self):
+        print("Efetuar Pagamento clicado!")
 
     def destruir(self):
         """Remove os widgets da tela"""
         self.frame.destroy()
 
 
-class App:
-    def __init__(self, root):
-        self.root = root
-        self.tela_atual = None
-        self.abrir_menu_principal()
-    
-    def abrir_menu_principal(self):
-        if self.tela_atual:
-            self.tela_atual.destruir()
-        self.atual = MenuPrincipal(self.root, self)
-    
-    def abrir_tela_cadastro(self):
-        print("tela Aberta")
-
-    def abrir_tela_Registados(self):
-        print("tela aberta")
-
-
 if __name__ == "__main__":
-   root = tk.Tk()
-   root.title("Sistema do Dizimo")
-   root.geometry("800x600")
-   root.resizable(False,False)
+    root = tk.Tk()
+    root.title("Menu Principal")
+    root.geometry("600x500")
+    root.resizable(False, False)
 
-   app = App(root)
-   root.mainloop()
-
-
-
-
-
+    menu = MenuPrincipal(root)
+    root.mainloop()

d419f7f5bf745b679870d9fb85a880284f77444f
danilo <danilosoaressantos4@gmail.com>
2025-01-10 11:19:42 -0300

    Adiçoes de novos campos em cadastro

--- Main.py/Cadastro.py
+++ Main.py/Cadastro.py
@@ -35,11 +35,19 @@ class TelaCadastro:
         self.data_nascimento = tk.Entry(self.campos_frame)
         self.telefone = tk.Entry(self.campos_frame)
         self.endereco = tk.Entry(self.campos_frame)
+        self.conjuge = tk.Entry(self.campos_frame)
+        self.data_nascimento_conjuge = tk.Entry(self.campos_frame)
+        self.paroquia = tk.Entry(self.campos_frame)
+        self.numero_inscrição = tk.Entry(self.campos_frame)
 
         self.atualizar_lista()
 
     def voltar_menu(self):
         MenuPrincipal(self.master)
+        self.frame.destroy()
+
+    def voltar_lista(self):
+        TelaCadastro(self.master, self.cadastro)
         self.frame.destroy()
 
     def atualizar_lista(self):
@@ -58,13 +66,22 @@ class TelaCadastro:
         tk.Label(self.campos_frame, text="Data Nascimento", font=("Arial", 12)).grid(row=1, column=0, pady=2, padx=10)
         tk.Label(self.campos_frame, text="Telefone", font=("Arial", 12)).grid(row=2, column=0, pady=2, padx=10)
         tk.Label(self.campos_frame, text="Endereço", font=("Arial", 12)).grid(row=3, column=0, pady=2, padx=10)
+        tk.Label(self.campos_frame, text="Conjuge", font=("Arial", 12)).grid(row=4, column=0, pady=2, padx=10)
+        tk.Label(self.campos_frame, text="Data de Nascimento Conjuge", font=("Arial", 12)).grid(row=5, column=0, pady=2, padx=10)
+        tk.Label(self.campos_frame, text="Paroquia", font=("Arial", 12)).grid(row=6, column=0, pady=2, padx=10)
+        tk.Label(self.campos_frame, text="Número de Inscrição", font=("Arial", 12)).grid(row=7, column=0, pady=2, padx=10)
 
         self.nome_completo.grid(row=0, column=1, pady=2, padx=10)
         self.data_nascimento.grid(row=1, column=1, pady=2, padx=10)
         self.telefone.grid(row=2, column=1, pady=2, padx=10)
         self.endereco.grid(row=3, column=1, pady=2, padx=10)
+        self.conjuge.grid(row=4, column=1, pady=2, padx=10)
+        self.data_nascimento_conjuge.grid(row=5, column=1, pady=2, padx=10)
+        self.paroquia.grid(row=6, column=1, pady=2, padx=10)
+        self.numero_inscrição.grid(row=7, column=1, pady=2, padx=10)
 
-        tk.Button(self.campos_frame, text="Salvar", command=self.salvar_cadastro).grid(row=4, column=0, columnspan=2, pady=10)
+        tk.Button(self.campos_frame, text="Salvar", command=self.salvar_cadastro).grid(row=8, column=0, columnspan=1, pady=10)
+        tk.Button(self.campos_frame, text="Voltar", command=self.voltar_lista).grid(row=8, column=0, columnspan=2, pady=10)
 
     def salvar_cadastro(self):
         """Salva um novo cadastro e exibe novamente a lista."""
@@ -72,8 +89,12 @@ class TelaCadastro:
         data_nascimento = self.data_nascimento.get().strip()
         telefone = self.telefone.get().strip()
         endereco = self.endereco.get().strip()
+        conjuge = self.conjuge.get().strip()
+        data_nascimento_conjuge = self.data_nascimento_conjuge.get().strip()
+        paroquia = self.paroquia.get().strip()
+        numero_inscrição = self.numero_inscrição.get().strip()
 
-        if not all([nome, data_nascimento, telefone, endereco]):
+        if not all([nome, data_nascimento, telefone, endereco, conjuge, data_nascimento_conjuge, paroquia, numero_inscrição]):
             messagebox.showerror("Erro", "Preencha todos os campos!")
             return
 
@@ -83,13 +104,17 @@ class TelaCadastro:
             messagebox.showerror("Erro", "Data de nascimento inválida! Use o formato DD/MM/AAAA.")
             return
 
-        self.cadastro.adicionar_registro(nome, data_nascimento, telefone, endereco)
+        self.cadastro.adicionar_registro(nome, data_nascimento, telefone, endereco, conjuge, data_nascimento_conjuge, paroquia, numero_inscrição)
         self.atualizar_lista()
 
         self.nome_completo.delete(0, tk.END)
         self.data_nascimento.delete(0, tk.END)
         self.telefone.delete(0, tk.END)
         self.endereco.delete(0, tk.END)
+        self.conjuge.delete(0, tk.END)
+        self.data_nascimento_conjuge.delete(0, tk.END)
+        self.paroquia.delete(0, tk.END)
+        self.numero_inscrição.delete(0, tk.END)
 
         messagebox.showinfo("Sucesso", "Cadastro efetuado com sucesso!")
 
@@ -128,12 +153,16 @@ class Cadastro:
     def __init__(self):
         self.registros = []
 
-    def adicionar_registro(self, nome, data_nascimento, telefone, endereco):
+    def adicionar_registro(self, nome, data_nascimento, telefone, endereco,conjuge, data_nascimento_conjuge, paroquia, numero_inscrição):
         registro = {
             "nome": nome,
             "data_nascimento": data_nascimento,
             "telefone": telefone,
-            "endereco": endereco
+            "endereco": endereco,
+            "conjuge": conjuge,
+            "data_nascimento_conjuge": data_nascimento_conjuge,
+            "paroquia": paroquia,
+            "numero_inscrição": numero_inscrição
         }
         self.registros.append(registro)
 
